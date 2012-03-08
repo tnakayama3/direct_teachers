3aad4dee5d2fafc1bacd808acc930910d0b71d64
Takefumi Nakayama <takefumi.cm@gmaiol.com>
2012-03-08 19:04:43 +0900

    initial commit of htmls

---
+++ 20_html/10_modified/scripts/swapImg.js
@@ -0,0 +1,24 @@
+function MM_preloadImages(){
+	var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+	var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+	if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore(){
+	var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d){
+	var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+	d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+	if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+	for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+	if(!x && document.getElementById) x=document.getElementById(n); return x;
+}
+
+function MM_swapImage(){
+	var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+	if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+MM_preloadImages('images/menu/menu_1_b.jpg','images/menu/menu_2_b.jpg','images/menu/menu_3_b.jpg','images/menu/menu_4_b.jpg','images/menu/menu_5_b.jpg','images/menu/menu_6_b.jpg','images/ad/ad1_2.jpg','images/ad/ad2_2.jpg','images/ad/ad3_2.jpg','images/btn_index_teacher1.gif','images/btn_index_teacher2.gif','images/btn_index_search_arrow1.gif','images/btn_index_search_arrow2.gif');
